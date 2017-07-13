867217153547cb0b9b35617dd2415cbe041f6f94
Carl Lofton <Carl.lofton@comcast.net>
2017-07-13 14:26:58 -0400

    Update csv2sqlite.py

--- Ingestion/csv2sqlite.py
+++ Ingestion/csv2sqlite.py
@@ -5,7 +5,7 @@ cur = con.cursor()
 cur.execute("CREATE TABLE t (searchid, pub, date, title, edition, journal, pubtype, highlight, byline, lang, section, load, length, Text);") # use your column names here
 
 
-with open('data.csv','rb') as fin: # `with` statement available in 2.5+
+with open('data.csv','rb') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
     to_db = [(i['searchid'], i['pub'], i['title'], i['edition'], i[journal], i[pubtype], i[highlight], i[byline], i[lang], i[section], i[load], i[length], i[Text]) for i in dr]
