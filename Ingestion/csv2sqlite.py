453f7f2d28c2df5b48cc7407bac53cb6f20ba649
Carl Lofton <Carl.lofton@comcast.net>
2017-07-13 14:26:11 -0400

    Update csv2sqlite.py

--- Ingestion/csv2sqlite.py
+++ Ingestion/csv2sqlite.py
@@ -2,15 +2,15 @@ import csv, sqlite3
 
 con = sqlite3.connect(":memory:")
 cur = con.cursor()
-cur.execute("CREATE TABLE t (searchid, pub, date, title, edition);") # use your column names here
+cur.execute("CREATE TABLE t (searchid, pub, date, title, edition, journal, pubtype, highlight, byline, lang, section, load, length, Text);") # use your column names here
 
 
 with open('data.csv','rb') as fin: # `with` statement available in 2.5+
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['searchid'], i['pub'], i['title'], i['edition']) for i in dr]
+    to_db = [(i['searchid'], i['pub'], i['title'], i['edition'], i[journal], i[pubtype], i[highlight], i[byline], i[lang], i[section], i[load], i[length], i[Text]) for i in dr]
 
-cur.executemany("INSERT INTO t (searchid, pub, date, title, edition) VALUES ((?, ?, ?, ?);", to_db)
+cur.executemany("INSERT INTO t (searchid, pub, date, title, edition, journal, pubtype, highlight, byline, lang, section, load, length, Text) VALUES ((?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 con.commit()
 con.close()
 
