a23a3db25f30511d1ce157ef568966b940fa9289
Carl Lofton <Carl.lofton@comcast.net>
2017-07-14 22:54:46 -0400

    Update Event-csv2sqlite.py
    created, corrected and tested

--- Ingestion/Event-csv2sqlite.py
+++ Ingestion/Event-csv2sqlite.py
@@ -2,13 +2,14 @@ import csv, sqlite3
 
 con = sqlite3.connect(":memory:")
 cur = con.cursor()
-cur.execute("CREATE TABLE Events (date, year, annot, event, source, url, severity);") # use your column names here
+cur.execute("CREATE TABLE Events (Adate, year, annotation, event, source, url);") # use your column names here
 
-with open('data.csv','rb') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
+with open('Syriantimeline.csv','rt') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['data'], i['year'], i['annot'], i['event'], i[source], i[url], i[severity]]) for i in dr]
+    to_db = [(i['Date'], i['Year'], i['Annotation'], i['Event'], i['Source'], i['Source URL']) for i in dr]
 
-cur.executemany("INSERT INTO t (date, year, annot, event, source, url, severity) VALUES ((?, ?, ?, ?, ?, ?, ?);", to_db)
+cur.executemany("INSERT INTO Events (Adate, year, annotation, event, source, url) VALUES (?, ?, ?, ?, ?, ?);", to_db)
 con.commit()
+#add code to create a column for severity
 con.close()
