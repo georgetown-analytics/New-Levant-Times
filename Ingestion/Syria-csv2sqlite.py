8cdd84b76b1ee38ae56738628afee6346e978bee
Carllofton <Carl.lofton@comcast.net>
2017-08-07 23:52:12 -0400

    updated syria lexis nexis to sqlite

--- Ingestion/Syria-csv2sqlite.py
+++ Ingestion/Syria-csv2sqlite.py
@@ -1,17 +1,24 @@
 import csv, sqlite3
 
-con = sqlite3.connect(":memory:")
+con = sqlite3.connect("SyriaText.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE t (searchid, publication, Adate, title, edition, journal, pubtype, highlight, byline, lang, section, load, length, aText);") # use your column names here
+cur.execute("CREATE TABLE t (publication, date, title, length, publicationtype, text, year, month, day);") # use your column names here
 
-with open('nexistestdata.csv','rt') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
+with open('CleanLexisNexis.csv','rt') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    print(dr)
-    wait = input("PRESS ENTER TO CONTINUE.")
-    to_db = [(i['SEARCH_ID'], i['PUBLICATION'], i['ADATE'], i['TITLE'], i['EDITION'], i['JOURNAL-CODE'], i['PUBLICATION-TYPE'], i['HIGHLIGHT'], i['BYLINE'], i['LANGUAGE'], i['SECTION'], i['LOAD-DATE'], i['LENGTH'], i['TEXT']) for i in dr]
+   
+   # wait = input("PRESS ENTER TO CONTINUE.")
+    to_db = [(i['publication'], i['date'], i['title'], i['length'], i['publicationtype'], i['text'], i['year'], i['month'], i['day']) for i in dr]
 
-cur.executemany("INSERT INTO t (searchid, publication, Adate, title, edition, journal, pubtype, highlight, byline, lang, section, load, length, aText) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+cur.executemany("INSERT INTO t (publication, date, title, length, publicationtype, text, year, month, day) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 con.commit()
+
+cur.execute('SELECT * FROM t group by Publication')
+rows = cur.fetchall()
+ 
+for row in rows:
+    print(row)
+
 con.close()
 
