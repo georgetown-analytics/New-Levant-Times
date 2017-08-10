7bb23119322929663a463083df2e68a75e239463
Carl Lofton <Carl.lofton@comcast.net>
2017-08-10 17:13:45 -0400

    Added latest timeline to database

--- Ingestion/Event-csv2sqlite.py
+++ Ingestion/Event-csv2sqlite.py
@@ -1,15 +1,22 @@
 import csv, sqlite3
 
-con = sqlite3.connect(":memory:")
+con = sqlite3.connect("EventsTimeline.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE Events (Adate, year, annotation, event, source, url);") # use your column names here
+cur.execute("CREATE TABLE Events (Date, EventText, Average, Mode);") 
 
-with open('Syriantimeline.csv','rt') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
+with open('EventsFinal.csv','rt') as fin: # `with` statement available in 2.5+ (data.csv is the data file)
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['Date'], i['Year'], i['Annotation'], i['Event'], i['Source'], i['Source URL']) for i in dr]
+    to_db = [(i['Date'], i['EventText'], i['Average'], i['Mode']) for i in dr]
 
-cur.executemany("INSERT INTO Events (Adate, year, annotation, event, source, url) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+cur.executemany("INSERT INTO Events (Date, EventText, Average, Mode) VALUES (?, ?, ?, ?);", to_db)
 con.commit()
 #add code to create a column for severity
+
+cur.execute('SELECT * FROM Events group by Mode')
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
 con.close()
